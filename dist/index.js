bfc353a06af0f35acc7518460d64b265f94da10c
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-03-31 18:51:47 +0200

    any type added

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,6 @@
 "use strict";
 //---------
-//INTRO
+// INTRO
 //---------
 /* let age: number = 30
 let firstName: string = 'Mario'
@@ -36,53 +36,82 @@ anotherThing = undefined */
 //---------
 // arrays
 //---------
-let names = ["Mario", "Luigi", "Peach"];
-let ages = [25, 35, 40];
-names.push("bowser");
-ages.push(44);
+// let names: string[] = ["Mario", "Luigi", "Peach"];
+// let ages: number[] = [25, 35, 40];
+// names.push("bowser");
+// ages.push(44);
 //------------------------------
 // type interference with arrays
 //------------------------------
-let fruits = ["apples", "pears", "bananas", "mangos"];
-fruits.push("peaches");
-const fruit = fruits[3];
-let things = [1, true, "hello"];
-const t = things[0];
+// let fruits: string[] = ["apples", "pears", "bananas", "mangos"];
+// fruits.push("peaches");
+// const fruit = fruits[3];
+// let things = [1, true, "hello"];
+// const t = things[0];
 //------------------
 // object literals & type interference with object literals
 //------------------
-let user = {
-    firstName: "mario",
-    age: 28,
-    id: 1,
-    //isFictional: true
-};
-user.firstName = "luigi";
-// user.firstName = false
-//user.email = 'peach@gmail.com'
-user.id = 2;
-const age = user.age;
+// let user: { firstName: string; age: number; id: number } = {
+//   firstName: "mario",
+//   age: 28,
+//   id: 1,
+//   //isFictional: true
+// };
+// user.firstName = "luigi";
+// // user.firstName = false
+// //user.email = 'peach@gmail.com'
+// user.id = 2;
+// const age = user.age;
 //------------
 // functions
 //------------
-function addTwoNumbers(a, b) {
-    return a + b;
-    //return true
-}
-const subtractTwoNumbers = (a, b) => {
-    return a - b;
-};
-addTwoNumbers(10, 10);
-subtractTwoNumbers(28, 12);
-function addAllNumbers(items) {
-    const total = items.reduce((a, b) => a + b, 0); //starting point is 0
-    console.log(total);
-}
-addAllNumbers([5, 5, 8]);
+// function addTwoNumbers(a: number, b: number): number {
+//   return a + b;
+//   //return true
+// }
+// const subtractTwoNumbers = (a: number, b: number): number => {
+//   return a - b;
+// };
+// addTwoNumbers(10, 10);
+// subtractTwoNumbers(28, 12);
+// function addAllNumbers(items: number[]): void {
+//   const total = items.reduce((a, b) => a + b, 0); //starting point is 0
+//   console.log(total);
+// }
+// addAllNumbers([5, 5, 8]);
 //-------------------------
 // return type interference
 //-------------------------
-function formatGreeting(name, greeting) {
-    return `${greeting}, ${name}`;
+// function formatGreeting(name: string, greeting: string): string {
+//   return `${greeting}, ${name}`;
+// }
+// const result = formatGreeting("mario", "hello");
+//----------
+// any type
+//----------
+let age;
+let title;
+age = 30;
+age = false;
+title = 25;
+title = "dada";
+title = {
+    name: "title",
+};
+//-------------------
+// any type in arrays
+//-------------------
+//let fruits = []
+let things = ["hello", true, 25, null];
+things.push({ id: 2 });
+//-------------------------
+// functions & any
+//-------------------------
+function addTogether(value) {
+    return value + value;
 }
-const result = formatGreeting("mario", "hello");
+const resultOne = addTogether(0.1);
+const resultTwo = addTogether("hello");
+//console.log(resultOne, resultTwo)
+//useful when migrating project from js to ts
+// bcs using any won't cause errors initially
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 //---------
-//INTRO
+// INTRO
 //---------
 
 /* let age: number = 30
@@ -38,73 +38,113 @@ anotherThing = undefined */
 // arrays
 //---------
 
-let names: string[] = ["Mario", "Luigi", "Peach"];
-let ages: number[] = [25, 35, 40];
+// let names: string[] = ["Mario", "Luigi", "Peach"];
+// let ages: number[] = [25, 35, 40];
 
-names.push("bowser");
-ages.push(44);
+// names.push("bowser");
+// ages.push(44);
 
 //------------------------------
 // type interference with arrays
 //------------------------------
 
-let fruits: string[] = ["apples", "pears", "bananas", "mangos"];
+// let fruits: string[] = ["apples", "pears", "bananas", "mangos"];
 
-fruits.push("peaches");
+// fruits.push("peaches");
 
-const fruit = fruits[3];
+// const fruit = fruits[3];
 
-let things = [1, true, "hello"];
+// let things = [1, true, "hello"];
 
-const t = things[0];
+// const t = things[0];
 
 //------------------
 // object literals & type interference with object literals
 //------------------
 
-let user: { firstName: string; age: number; id: number } = {
-  firstName: "mario",
-  age: 28,
-  id: 1,
-  //isFictional: true
-};
+// let user: { firstName: string; age: number; id: number } = {
+//   firstName: "mario",
+//   age: 28,
+//   id: 1,
+//   //isFictional: true
+// };
 
-user.firstName = "luigi";
-// user.firstName = false
-//user.email = 'peach@gmail.com'
-user.id = 2;
+// user.firstName = "luigi";
+// // user.firstName = false
+// //user.email = 'peach@gmail.com'
+// user.id = 2;
 
-const age = user.age;
+// const age = user.age;
 
 //------------
 // functions
 //------------
 
-function addTwoNumbers(a: number, b: number): number {
-  return a + b;
-  //return true
-}
+// function addTwoNumbers(a: number, b: number): number {
+//   return a + b;
+//   //return true
+// }
 
-const subtractTwoNumbers = (a: number, b: number): number => {
-  return a - b;
-};
+// const subtractTwoNumbers = (a: number, b: number): number => {
+//   return a - b;
+// };
 
-addTwoNumbers(10, 10);
-subtractTwoNumbers(28, 12);
+// addTwoNumbers(10, 10);
+// subtractTwoNumbers(28, 12);
 
-function addAllNumbers(items: number[]): void {
-  const total = items.reduce((a, b) => a + b, 0); //starting point is 0
-  console.log(total);
-}
+// function addAllNumbers(items: number[]): void {
+//   const total = items.reduce((a, b) => a + b, 0); //starting point is 0
+//   console.log(total);
+// }
 
-addAllNumbers([5, 5, 8]);
+// addAllNumbers([5, 5, 8]);
 
 //-------------------------
 // return type interference
 //-------------------------
 
-function formatGreeting(name: string, greeting: string): string{
-  return `${greeting}, ${name}`;
+// function formatGreeting(name: string, greeting: string): string {
+//   return `${greeting}, ${name}`;
+// }
+
+// const result = formatGreeting("mario", "hello");
+
+//----------
+// any type
+//----------
+
+let age: any;
+let title;
+
+age = 30;
+age = false;
+
+title = 25;
+title = "dada";
+title = {
+  name: "title",
+};
+
+//-------------------
+// any type in arrays
+//-------------------
+
+//let fruits = []
+let things: any[] = ["hello", true, 25, null];
+
+things.push({ id: 2 });
+
+//-------------------------
+// functions & any
+//-------------------------
+
+function addTogether(value: any): any {
+  return value + value;
 }
 
-const result = formatGreeting("mario", "hello");
+const resultOne = addTogether(0.1);
+const resultTwo = addTogether("hello");
+//console.log(resultOne, resultTwo)
+
+//useful when migrating project from js to ts
+// bcs using any won't cause errors initially
