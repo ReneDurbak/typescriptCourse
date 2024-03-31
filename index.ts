a23f8fb1658b5c4a591b4714c689ca2c746e6f4f
René Durbák <106143298+ReneDurbak@users.noreply.github.com>
2024-03-31 17:23:24 +0200

    type basics

--- index.ts
+++ index.ts
@@ -1,2 +1,32 @@
-let age = 30
-console.log(30)
+let age: number = 30
+let firstName: string = 'Mario'
+let isFictional: boolean
+
+age = 32
+firstName = 'Luigi'
+//isFictional = 'false'
+isFictional = true
+
+
+
+let planet = 'Earth'
+let moons = 1
+let isLarge = false
+
+planet = 'Saturn'
+//moons = '145'
+moons = 145
+//isLarge = 'yes'
+isLarge = true
+
+
+
+
+let something: null
+let anotherThing: undefined
+
+//something = 10
+something = null
+//anotherThing = 'hello'
+anotherThing = undefined
+
